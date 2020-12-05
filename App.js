d09be978a8eab80684a180df62102555ab86acf0
Jordan Borja <www.jrdnbrj@gmail.com>
2020-12-04 23:44:56 -0500

    Change library and functional app

--- App.js
+++ App.js
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react'
 import {
   StyleSheet,
   ScrollView,
   View,
   Text,
   StatusBar,
-} from 'react-native';
-
+} from 'react-native'
+// import Geolocation from '@react-native-community/geolocation'
+import Geolocation from 'react-native-geolocation-service'
 
 const App: () => React$Node = () => {
+  
+  const [accuracy, setAccuracy] = useState('')
+  const [altitude, setAltitude] = useState('')
+  const [latitude, setLatitude] = useState('')
+  const [longitude, setLongitude] = useState('')
+
+  const getLocation = () => {
+    Geolocation.getCurrentPosition(info => {
+      console.log(info)
+      setAccuracy(info.coords.accuracy)
+      setAltitude(info.coords.altitude)
+      setLatitude(info.coords.latitude)
+      setLongitude(info.coords.longitude)
+    });
+  }
+
+  getLocation()
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <Text>Aquefuemen</Text>
+      <Text>Exactitud: { accuracy }</Text>
+      <Text>Altitud: { altitude }</Text>
+      <Text>Latitud: { latitude }</Text>
+      <Text>Longitud: { longitude }</Text>
     </>
   );
 };

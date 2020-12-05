5f9420f76ed95dd8554f0f70e8a5620ca2b10401
Jordan Borja <www.jrdnbrj@gmail.com>
2020-12-05 01:51:00 -0500

    Added Marks

--- App.js
+++ App.js
@@ -1,26 +1,15 @@
 import React, { useState } from 'react'
-import {
-  StyleSheet,
-  Text,
-  View,
-  Dimensions,
-  StatusBar
-} from 'react-native'
+import { Dimensions } from 'react-native'
 import Geolocation from 'react-native-geolocation-service'
-import MapView from 'react-native-maps'
+import MapView, { Marker } from 'react-native-maps'
 
 const App: () => React$Node = () => {
   
-  const [accuracy, setAccuracy] = useState('')
-  const [altitude, setAltitude] = useState('')
   const [latitude, setLatitude] = useState('')
   const [longitude, setLongitude] = useState('')
 
   const getLocation = () => {
     Geolocation.getCurrentPosition(info => {
-      console.log('info:', info)
-      setAccuracy(info.coords.accuracy)
-      setAltitude(info.coords.altitude)
       setLatitude(info.coords.latitude)
       setLongitude(info.coords.longitude)
     });
@@ -29,45 +18,30 @@ const App: () => React$Node = () => {
   getLocation()
   const { height, width } = Dimensions.get('window');
   return <>
-      <StatusBar hidden={true}/>
-      {/* <Text>Exactitud: { accuracy }</Text>
-      <Text>Altitud: { altitude }</Text>
-      <Text>Latitud: { latitude }</Text>
-      <Text>Longitud: { longitude }</Text> */}
-      { latitude && longitude ? 
-        <View style={styles.container}>
-          <MapView
-            style={styles.map}
-            initialRegion={{
-              latitude,
-              longitude,
-              latitudeDelta: 0.0922,
-              longitudeDelta: 0.0421,
-            }}
-            /> 
-        </View>
-        : null
-      }
-    </>
+    { latitude && longitude ? 
+        <MapView
+          style={{ height, width }}
+          initialRegion={{
+            latitude,
+            longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          <Marker
+            coordinate={{ latitude, longitude }}
+            title='Casa de Jordan'
+            description='Aquí fue donde Jordan realizó este deber'
+          />
+          <Marker
+            coordinate={{ latitude: -0.16924030945500612, longitude: -78.47092322469005 }}
+            title='UDLA'
+            description='Aquí es donde Jordan dice que estudia'
+          />
+        </MapView>
+      : null
+    }
+  </>
 }
-
-const styles = StyleSheet.create({
-  container: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  map: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-  },
-});
 
 export default App;

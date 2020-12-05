7315a436da2201b7cf135dd40c6958dc94e827f5
Jordan Borja <www.jrdnbrj@gmail.com>
2020-12-04 23:04:05 -0500

    Project Created

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <Text>Aquefuemen</Text>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
